978ae1c90a65d87838c87c3bea09d89546046f85
A36-PC26\alumnat <a@a.com>
2025-02-21 17:28:11 +0100

    funcion para ordenador por orden descendente

--- Proyecto_diseño_modular/src/pac/GestorDeAplicacion.java
+++ Proyecto_diseño_modular/src/pac/GestorDeAplicacion.java
@@ -9,10 +9,40 @@ public class GestorDeAplicacion {
         }
     }
 }
-public void buscarPorOrigen(Producto[] productos, String origen) {
+	public void buscarPorOrigen(Producto[] productos, String origen) {
     for (int i = 0; i < productos.length; i++) {
         if (productos[i].obtenerOrigen().toLowerCase().equals(origen.toLowerCase())) {
             System.out.println("Producto encontrado: " + productos[i].obtenerNombre());
         }
     }
 }
+	public void ordenarPorPrecio(Producto[] productos) {
+    Producto productoTemporal;
+    for (int i = 0; i < productos.length - 1; i++) {
+        for (int j = i + 1; j < productos.length; j++) {
+            if (productos[i].obtenerPrecio() > productos[j].obtenerPrecio()) {
+                productoTemporal = productos[i];
+                productos[i] = productos[j];
+                productos[j] = productoTemporal;
+            }
+        }
+    }
+    System.out.println("Productos ordenados por precio de menor a mayor:");
+    mostrarProductos(productos);
+}
+	public void ordenarPorPrecioDescendente(Producto[] productos) {
+	    Producto productoTemporal;
+	    for (int i = 0; i < productos.length - 1; i++) {
+	        for (int j = i + 1; j < productos.length; j++) {
+	            if (productos[i].obtenerPrecio() < productos[j].obtenerPrecio()) {
+	                productoTemporal = productos[i];
+	                productos[i] = productos[j];
+	                productos[j] = productoTemporal;
+	            }
+	        }
+	    }
+	    System.out.println("Productos ordenados por precio de mayor a menor:");
+	    mostrarProductos(productos);
+	}
+
+
