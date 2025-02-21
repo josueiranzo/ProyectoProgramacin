9f9f5e7dda08e05081f0f0b3d6e46de54778103f
A36-PC26\alumnat <a@a.com>
2025-02-21 17:13:57 +0100

    funcion para ordenador los productos por precio en acendentes

--- Proyecto_diseño_modular/src/pac/GestorDeAplicacion.java
+++ Proyecto_diseño_modular/src/pac/GestorDeAplicacion.java
@@ -1,6 +1,6 @@
 package pac;
 
-public class GestorDeAplicacion {
+	public class GestorDeAplicacion {
     public void buscarPorNombre(Producto[] productos, String nombre) {
         for (int i = 0; i < productos.length; i++) {
             if (productos[i].obtenerNombre().toLowerCase().contains(nombre.toLowerCase())) {
@@ -9,10 +9,25 @@ public class GestorDeAplicacion {
         }
     }
 }
-public void buscarPorOrigen(Producto[] productos, String origen) {
+	public void buscarPorOrigen(Producto[] productos, String origen) {
     for (int i = 0; i < productos.length; i++) {
         if (productos[i].obtenerOrigen().toLowerCase().equals(origen.toLowerCase())) {
             System.out.println("Producto encontrado: " + productos[i].obtenerNombre());
         }
     }
+}
+	public void ordenarPorPrecio(Producto[] productos) {
+    Producto productoTemporal;
+    for (int i = 0; i < productos.length - 1; i++) {
+        for (int j = i + 1; j < productos.length; j++) {
+            if (productos[i].obtenerPrecio() > productos[j].obtenerPrecio()) {
+                // Intercambiar los productos directamente sin usar 'temp'
+                productoTemporal = productos[i];
+                productos[i] = productos[j];
+                productos[j] = productoTemporal;
+            }
+        }
+    }
+    System.out.println("Productos ordenados por precio de menor a mayor:");
+    mostrarProductos(productos);
 }
