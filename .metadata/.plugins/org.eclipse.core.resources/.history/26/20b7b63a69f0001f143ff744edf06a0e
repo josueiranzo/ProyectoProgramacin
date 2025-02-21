e31b219ed009adc67f5cdc014d1515b01dc44fe0
A36-PC26\alumnat <a@a.com>
2025-02-21 16:35:23 +0100

    Agregar controlador de los productos , para que no pase de 10 productos-

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/26/20b7b63a69f0001f143ff744edf06a0e
@@ -0,0 +1,8 @@
+package pac;
+
+public class Carrito {
+    Producto[] productos = new Producto[10]; 
+    double[] cantidades = new double[10];  
+    int contadorCarrito = 0;  
+}
+
--- Proyecto_diseño_modular/src/pac/Carrito.java
+++ Proyecto_diseño_modular/src/pac/Carrito.java
@@ -1,8 +1,17 @@
 package pac;
 
 public class Carrito {
-    Producto[] productos = new Producto[10]; 
+    Producto[] productos = new Producto[10];  
     double[] cantidades = new double[10];  
-    int contadorCarrito = 0;  
-}
+    int contadorCarrito = 0; 
 
+    public void agregarProducto(Producto producto, double cantidad) {
+        if (contadorCarrito < productos.length) {
+            productos[contadorCarrito] = producto;
+            cantidades[contadorCarrito] = cantidad;
+            contadorCarrito++;
+            System.out.println("Producto añadido al carrito: " + producto.obtenerNombre() + " - Cantidad: " + cantidad);
+        } else {
+            System.out.println("El carrito está lleno.");
+        }
+    }
